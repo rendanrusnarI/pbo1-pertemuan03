ec7673e13f49dac081f2213bfbb667440ccb317b
rendan <rendanrusnari@gmail.com>
2021-04-22 13:38:27 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class OperatorBinary {
+    String nama = "Rendan Rusnari";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.55;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
